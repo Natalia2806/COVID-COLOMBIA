39f21161f8e543426a3f1f0751c757e06ac8d408
Natalia2806 <91932527+NataliaTuiran2806@users.noreply.github.com>
2022-06-01 20:19:02 -0500

    Eliminamos las columnas del dataSet

--- COVID-COLOMBIA.py
+++ COVID-COLOMBIA.py
@@ -1,3 +1,19 @@
 """
 ESTUDIANTE: NATALIA TUIRAN QUINTERO
 """
+import pandas as pd
+
+#DATASETS
+url = 'Casos_positivos_de_COVID-19_en_Colombia.csv'
+data = pd.read_csv(url)[:3000]
+
+# ELIMINAMOS LAS COLUMNAS DEL DATASET 
+data.drop('Código ISO del país', axis = 1, inplace=True)
+data.drop('Nombre del país', axis = 1, inplace=True)
+data.drop('Pertenencia étnica', axis = 1, inplace=True)
+data.drop('Nombre del grupo étnico', axis = 1, inplace=True)
+data.drop('Fecha de inicio de síntomas', axis = 1, inplace=True)
+data.drop('Unidad de medida de edad', axis = 1, inplace=True)
+data.drop('Código DIVIPOLA departamento', axis = 1, inplace=True)
+data.drop('Código DIVIPOLA municipio', axis = 1, inplace=True)
+data.drop('ID de caso', axis = 1, inplace=True)
